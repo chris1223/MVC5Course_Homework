9704f96fa7030d46f4688f0ba53dd1ff98836b95
Chris Kao <chris711223@gmail.com>
2015-04-18 14:52:12 +0800

    init

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //hack 123
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            // TODO:程式尚未完成，請下一位接手
+            ViewBag.Message = "Your application description page.";
+            
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ProFile()
+        {//XX
+            return View();
+        }
+    }
+}
